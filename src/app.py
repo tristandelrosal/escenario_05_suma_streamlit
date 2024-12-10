bfd45050899eca206fbbe347faaceb8b6ce259c2
Tristan Del Rosal <tristandelrosal@gmail.com>
2024-12-10 11:03:26 +0100

    primea subida

---
+++ src/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("¡Calculadora!")
+
+st.write("Esto es una calculadora muy sencilla que suma dos números.")
+
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Escribe un numero..."
+)
+
+number2 = st.number_input(
+    "Insert a second number", value=None, placeholder="Escribe un numero..."
+)
+
+result = number + number2
+st.write("El resultado de la suma es: ", result)
