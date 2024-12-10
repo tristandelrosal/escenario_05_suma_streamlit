a0315363d157858718ea5b8005af6ab355b2e7d6
Tristan Del Rosal <tristandelrosal@gmail.com>
2024-12-10 11:18:45 +0100

    arreglo 3

--- src/app.py
+++ src/app.py
@@ -3,10 +3,7 @@ import streamlit as st
 st.title("Calculadora")
 
 st.write("Esto es una calculadora muy sencilla que suma dos números.")
-if number is None:
-  number = 0
-if number2 is None:
-  number2 = 0
+
 number = st.number_input(
     "Insert a number", value=None, placeholder="Escribe un numero..."
 )
@@ -14,7 +11,10 @@ number = st.number_input(
 number2 = st.number_input(
     "Insert a second number", value=None, placeholder="Escribe un numero..."
 )
-
+if number is None:
+  number = 0
+if number2 is None:
+  number2 = 0
 result = number + number2
 if result:
      st.write("El resultado de la suma es: ", result)
